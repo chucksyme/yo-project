4422c8e6d10da66c5e488d64c541ffc593dd6b13
chucksyme <chuck.syme@gmail.com>
2015-10-30 10:27:53 -0700

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoProjectApp
+ */
+angular.module('yoProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
